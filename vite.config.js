c8550f67314f75725a406ce5eb206ebd74f8e531
Madboy21 <justinjordan2211@gmail.com>
2025-08-19 19:30:47 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  root: ".", // frontend folder root
   plugins: [react()],
+  root: ".",       // frontend root
   build: {
-    outDir: "dist", // Vercel এর জন্য dist folder
-    rollupOptions: {
-      input: "/index.html"
-    }
-  }
+    outDir: "dist"
+  },
 });
+
