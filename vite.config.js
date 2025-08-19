5a40bcad1997918f95e1fc6854c6aaa266fc70db
Madboy21 <justinjordan2211@gmail.com>
2025-08-19 19:49:17 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist",
     rollupOptions: {
-      input: "./index.html"  // ensures index.html is the entry
-    }
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000", // local backend
-        changeOrigin: true,
-        rewrite: (p) => p.replace(/^\/api/, "")
-      }
+      input: "/index.html"
     }
   }
 });
